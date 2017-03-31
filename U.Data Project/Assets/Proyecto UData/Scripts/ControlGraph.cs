c70053b42ee9e23d806bc8e37a551a8687e960d2
Moon Antonio <pinchodev91@gmail.com>
2017-03-31 23:02:37 +0200

    Estructura del codigo

--- U.Data Project/Assets/Proyecto UData/Scripts/ControlGraph.cs
+++ U.Data Project/Assets/Proyecto UData/Scripts/ControlGraph.cs
@@ -9,6 +9,8 @@
 
 #region Librerias
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
 #endregion
 
 namespace MoonAntonio
@@ -18,6 +20,50 @@ namespace MoonAntonio
 	/// </summary>
 	public class ControlGraph : MonoBehaviour 
 	{
+		#region Variables Publicas
+		/// <summary>
+		/// <para>Valores del graph.</para>
+		/// </summary>
+		public List<float> valores = new List<float>();			// Valores del graph
+		/// <summary>
+		/// <para>Colores del graph.</para>
+		/// </summary>
+		public List<Color> colores = new List<Color>();			// Colores del graph
+		/// <summary>
+		/// <para>Prefab de la pieza del graph.</para>
+		/// </summary>
+		public Image prefab;                                    // Prefab de la pieza del graph
+		#endregion
 
+		#region Init
+		/// <summary>
+		/// <para>Init de ControlGraph.</para>
+		/// </summary>
+		private void Start()// Init de ControlGraph
+		{
+			// Inicializar Graph
+			CrearGraph();
+		}
+		#endregion
+
+		#region API
+		/// <summary>
+		/// <para>Agrega una nueva pieza al graph.</para>
+		/// </summary>
+		public void AddPieza()// Agrega una nueva pieza al graph
+		{
+
+		}
+		#endregion
+
+		#region Metodos
+		/// <summary>
+		/// <para>Crea un graph.</para>
+		/// </summary>
+		private void CrearGraph()// Crea un graph
+		{
+
+		}
+		#endregion
 	}
 }
