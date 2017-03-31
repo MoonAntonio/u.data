a091c0c0a214305ce376e0f372701a64e2140cec
Moon Antonio <pinchodev91@gmail.com>
2017-03-31 22:48:52 +0200

    Add PiezaGraph.cs

---
+++ U.Data Project/Assets/Proyecto UData/Scripts/PiezaGraph.cs
@@ -0,0 +1,25 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// PiezaGraph.cs (31/03/2017)													\\
+// Autor: Antonio Mateo (Moon Antonio) 									        \\
+// Descripcion:		Clase de la pieza graph										\\
+// Fecha Mod:		31/03/2017													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+using UnityEngine.UI;
+#endregion
+
+namespace MoonAntonio
+{
+	/// <summary>
+	/// <para>Clase de la pieza graph</para>
+	/// </summary>
+	[RequireComponent(typeof(Image))]
+	public class PiezaGraph : MonoBehaviour 
+	{
+
+	}
+}
