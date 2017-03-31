eae7c1b7af91a55cca76e3d000efc96b242cca40
Moon Antonio <pinchodev91@gmail.com>
2017-03-31 22:53:47 +0200

    Add ControlGraph.cs

---
+++ U.Data Project/Assets/Proyecto UData/Scripts/ControlGraph.cs
@@ -0,0 +1,23 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// ControlGraph.cs (31/03/2017)													\\
+// Autor: Antonio Mateo (Moon Antonio) 									        \\
+// Descripcion:		Control del graph											\\
+// Fecha Mod:		31/03/2017													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace MoonAntonio
+{
+	/// <summary>
+	/// <para>Control del graph</para>
+	/// </summary>
+	public class ControlGraph : MonoBehaviour 
+	{
+
+	}
+}
